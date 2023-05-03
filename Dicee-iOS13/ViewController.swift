b446c3c19156b69f97226496da0220a9e6e932e4
Josh Williams <joshua360x@gmail.com>
2023-05-02 18:05:26 -0700

    dice app

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+//    var leftDiceNumber = Int.random(in: 0...5)
+//    var rightDiceNumber = Int.random(in: 0...5)
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Who        . What  =  Value
+//        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+//        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+        
+//        diceImageView1.alpha = 0.5
+        
+        
     }
 
 
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+//        print("button got tapped")
+//        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
+//        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        // these next lines are the same thing. random Element automatically sees the elements already there and seeks through
+//        diceImageView1.image = diceArray[Int.random(in: 0...5)]
+        diceImageView1.image = diceArray.randomElement()
+        
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
+        
+//        leftDiceNumber += 1
+//        rightDiceNumber = rightDiceNumber - 1
+        
+//        print(Int.random(in: 1...10))
+
+    }
 }
 
